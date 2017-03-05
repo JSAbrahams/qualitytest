a8e25017aa9b35a9f8e24af0fd76e1093b17e5c1
Joel Abrahams <abrahamsjo@gmail.com>
2017-03-05 12:18:56 +0100

    Create rudimentary state pattern

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,21 +1,32 @@
 class QuestionsController < ApplicationController
   def new
     @type = params[:semantic]
+    @state = :init_state
   end
 
-  # Load the next page
-  def update
-    redirect_to(distortionVisible_path)
-    redirect_to(semanticRecognition_path)
-    redirect_to(describeObject_path)
+  def next_page
+    # state pattern would be much better, this is a bit ugly
+    if state == :init_state
+      render :distortionVisible_url
+      @state = :distortion_visible_state
 
-    if (@type == "indoor")
-      redirect_to(indoorDetails_path)
-    elsif (@type == "outdoor_man_made")
-      redirect_to(outdoorManMade_path)
-    else
-      redirect_to(outdoorNatural_path)
+    elsif state == :distortion_visible_state
+      render :semanticRecognition_path
+      @state = :semantic_recognition_state
+
+    elsif state == :semantic_recognition_state
+      if type == :indoor
+        render :indoorDetails_path
+      elsif type == :outdoor_natural
+        render :outdoorNatural_path
+      elsif type == :outdoor_man_made
+        render :outdoorManMade_path
+      end
+      @state = :describe_details_state
+
+    elsif state == :describe_details_state
+      render :describeObject_path
+      @state = :finished
     end
   end
-
 end
