6df75e4527550f6697583f5214c601530e3807cc
Joel Abrahams <abrahamsjo@gmail.com>
2017-03-09 22:46:23 +0100

    Try something with injection (yield)

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,32 +1,43 @@
 class QuestionsController < ApplicationController
-  def new
-    @type = params[:semantic]
-    @state = :init_state
-    render :layout => 'SemanticRecognition'
-  end
+  @type = params[:semantic]
+  @state = :init_state
+  @question = :dynamicPage
 
   def next_page
     # state pattern would be much better, this is a bit ugly
     if state == :init_state
-      render :layout => 'semanticRecognition'
       @state = :distortion_visible_state
+    elsif state == :distortion_visible_state
+      @state = :semantic_recognition_state
+    elsif state == :semantic_recognition_state
+      @state = :describe_details_state
+    elsif state == :describe_details_state
+      @state = :finished
+    end
+  end
+
+  protected
+
+  def dynamicPage
+    # state pattern would be much better, this is a bit ugly
+    if state == :init_state
+      'semanticRecognition'
 
     elsif state == :distortion_visible_state
-      render :layout => 'distortionVisible'
-      @state = :semantic_recognition_state
+      'distortionVisible'
 
     elsif state == :semantic_recognition_state
       if type == :indoor
-        render :layout => 'indoorDetails'
+        'indoorDetails'
       elsif type == :outdoor_natural
-        render :layout => 'outdoorNaturalDetails'
+        'outdoorNaturalDetails'
       elsif type == :outdoor_man_made
-        render :layout => 'outdoorManMadeDetails'
+        'outdoorManMadeDetails'
       end
       @state = :describe_details_state
 
     elsif state == :describe_details_state
-      render :ready_path
+      'done' # will give an error
       @state = :finished
     end
   end
