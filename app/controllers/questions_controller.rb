7b09dd7ea4473489653d3d493871204d34c51c5a
Joel Abrahams <abrahamsjo@gmail.com>
2017-03-10 15:18:30 +0100

    Move rending to questions.erb
    Almost there. I just need to figure out how to pass the state param and reload the page.

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,43 +1,18 @@
 class QuestionsController < ApplicationController
-  @type = params[:semantic]
-  @state = :init_state
-  @question = :dynamicPage
+  def new
+    @type = params[:semantic]
+    @state = "init_state"
+  end
 
   def next_page
     # state pattern would be much better, this is a bit ugly
-    if state == :init_state
+    if @state == "init_state"
       @state = :distortion_visible_state
-    elsif state == :distortion_visible_state
+    elsif @state == "distortion_visible_state"
       @state = :semantic_recognition_state
-    elsif state == :semantic_recognition_state
+    elsif @state == "semantic_recognition_state"
       @state = :describe_details_state
-    elsif state == :describe_details_state
-      @state = :finished
-    end
-  end
-
-  protected
-
-  def dynamicPage
-    # state pattern would be much better, this is a bit ugly
-    if state == :init_state
-      'semanticRecognition'
-
-    elsif state == :distortion_visible_state
-      'distortionVisible'
-
-    elsif state == :semantic_recognition_state
-      if type == :indoor
-        'indoorDetails'
-      elsif type == :outdoor_natural
-        'outdoorNaturalDetails'
-      elsif type == :outdoor_man_made
-        'outdoorManMadeDetails'
-      end
-      @state = :describe_details_state
-
-    elsif state == :describe_details_state
-      'done' # will give an error
+    elsif @state == "describe_details_state"
       @state = :finished
     end
   end
