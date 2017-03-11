96add1caa44eee03763c2264099a409b93d1bbdf
Joel Abrahams <abrahamsjo@gmail.com>
2017-03-11 11:21:16 +0100

    Add is training state

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -7,13 +7,19 @@ class QuestionsController < ApplicationController
   def next_page
     # state pattern would be much better, this is a bit ugly
     if @state == "init_state"
-      @state = :distortion_visible_state
+      @state = "distortion_visible_state"
     elsif @state == "distortion_visible_state"
-      @state = :semantic_recognition_state
+      @state = "semantic_recognition_state"
     elsif @state == "semantic_recognition_state"
-      @state = :describe_details_state
+      @state = "describe_details_state"
     elsif @state == "describe_details_state"
-      @state = :finished
+      @state = "finished"
+      if TrainingController.isTraining
+        TrainingController.setTraining(false)
+        redirect_to(ready_path)
+      else
+        redirect_to(new_images_path)
+      end
     end
   end
 end
--- app/controllers/training_controller.rb
+++ app/controllers/training_controller.rb
@@ -1,3 +1,15 @@
 class TrainingController < ApplicationController
+  def new
+    @training = true
+  end
+
+  public
+  def self.isTraining
+    @training
+  end
+
+  def self.setTraining(training)
+    @training = training
+  end
 
 end
