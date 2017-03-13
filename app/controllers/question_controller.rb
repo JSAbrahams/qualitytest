3a9aecffaf3864e61f3e1495137cd5e89ec827d6
Joel Abrahams <abrahamsjo@gmail.com>
2017-03-13 16:31:44 +0100

    Set correct state in question controller

--- app/controllers/question_controller.rb
+++ app/controllers/question_controller.rb
@@ -4,23 +4,33 @@ class QuestionController < ApplicationController
   end
 
   def question
+    session[:state] =
+        case session[:state]
+          when 'distortion_visible' then
+            'semantic_recognition'
+          when 'semantic_recognition' then
+            case session[:type]
+              when 'indoor' then
+                'indoor_detail'
+              when 'outdoor_natural' then
+                'outdoor_natural_detail'
+              else
+                'outdoor_man_made_detail'
+            end
+          when 'indoor_detail', 'outdoor_natural_detail', 'outdoor_man_made_detail' then
+            'describe_object'
+          when 'describe_object'
+            # question done
+            if TrainingController.is_training
+              TrainingController.set_training(false)
+              render ready_path
+            else
+              render new_images_path
+            end
+          else
+            'distortion_visible'
+        end
+
     @state = session[:state]
-    session[:state] =case session[:state]
-                       when 'distortion visible', NIL then
-                         'semantic_recognition'
-                       when 'semantic_recognition' then
-                         case session[:type]
-                           when 'indoor' then
-                             'indoor_detail'
-                           when 'outdoor_natural' then
-                             'outdoor_natural_detail'
-                           else
-                             'outdoor_man_made_detail'
-                         end
-                       when 'indoor_detail', 'outdoor_natural_detail', 'outdoor_man_made_detail' then
-                         'describe_object'
-                       else
-                         'question_done'
-                     end
   end
 end
--- app/controllers/training_controller.rb
+++ app/controllers/training_controller.rb
@@ -1,9 +1,9 @@
 class TrainingController < ApplicationController
-  def self.is_training
+  def is_training
     @training = session[:training].nil? ? false : session[:training]
   end
 
-  def self.set_training(training)
+  def set_training(training)
     session[:training] = training
   end
 
