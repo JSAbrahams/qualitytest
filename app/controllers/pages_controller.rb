8cfa043fcab945f60e9040b513a534e40a3ada94
Joel Abrahams <abrahamsjo@gmail.com>
2017-03-26 11:22:10 +0200

    Put back if statement in pages controller

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,36 +1,48 @@
 class PagesController < ApplicationController
   def index
     @title = "Home"
-    session[:campaign] = CampaignSet.order(:views).first.id
-    campaign = CampaignSet.find_by(id: session[:campaign])
-    campaign.started = campaign.started + 1
-    campaign.save
+    if params[:user].nil? && params[:campaign].nil? && params[:mw].nil?
+      session[:campaign] = CampaignSet.order(:completed).first.id
+      campaign = CampaignSet.find_by(id: session[:campaign])
+      campaign.started = campaign.started + 1
+      campaign.save
 
-    session[:userid] = User.count + 1
-    session[:image_viewtime_ids] = CampaignSet.find(session[:campaign]).image_viewtimes_id
+      session[:userid] = User.count + 1
+      session[:image_viewtime_ids] = CampaignSet.find(session[:campaign]).image_viewtimes_id
 
-    session[:validation] = 1
-    session[:question] = NIL
-    session[:training] = true
-    params[:semantic] = NIL
+      session[:validation] = 1
+      session[:question] = NIL
+      session[:training] = true
+      params[:semantic] = NIL
 
-    session[:img_num] = '-1'
+      session[:img_num] = '-1'
 
-    session[:image_ids] = []
-    session[:view_time_ids] = []
-    @image_viewtimes = session[:image_viewtime_ids].split(' ').shuffle
-    @image_viewtimes.each { |image_viewtime_id|
-      @image_viewtime = ImageViewtime.find(image_viewtime_id)
-      session[:image_ids].push @image_viewtime.image_id
-      session[:view_time_ids].push @image_viewtime.viewtime_id
-    }
+      session[:image_ids] = []
+      session[:view_time_ids] = []
+      @image_viewtimes = session[:image_viewtime_ids].split(' ').shuffle
+      @image_viewtimes.each { |image_viewtime_id|
+        @image_viewtime = ImageViewtime.find(image_viewtime_id)
+        session[:image_ids].push @image_viewtime.image_id
+        session[:view_time_ids].push @image_viewtime.viewtime_id
+      }
 
-    puts 'User user_id: ' + session[:userid].to_s
-    puts 'Campaign : ' + session[:campaign].to_s + ' : ' + @image_viewtimes.to_s
-    puts 'Image ids: ' + session[:image_ids].to_s
-    puts 'Presentation time ids: [' + session[:view_time_ids].to_s
+      puts 'User user_id: ' + session[:userid].to_s
+      puts 'Campaign : ' + session[:campaign].to_s + ' : ' + @image_viewtimes.to_s
+      puts 'Image ids: ' + session[:image_ids].to_s
+      puts 'Presentation time ids: [' + session[:view_time_ids].to_s
 
-    redirect_to newuser_path
+      redirect_to newuser_path
+    else
+      session[:campaign] = 2 # session[:campaign] = params[:campaign]
+      session[:userid] = params[:user].to_s
+      @finalstring = params[:mw] += params[:user] += "8c25a8f5e42c00a6f814f45ac764084f4b20a5c476be47ac2a674b82d0ba541f"
+      session[:vcode] = Digest::SHA2.hexdigest(@finalstring)
+      session[:vcode] = "mw-" + session[:vcode].to_s
+      session[:images] = CampaignSet.find(session[:campaign]).images
+      session[:scale] = CampaignSet.find(session[:campaign]).scale
+      session[:microworkers] = '1'
+      session[:content] = nil
+    end
   end
 
   def intro
@@ -50,7 +62,7 @@ class PagesController < ApplicationController
 
   def end
     campaign = CampaignSet.find_by(id: session[:campaign])
-    campaign.views = campaign.views + 1
+    campaign.views = campaign.completed + 1
     campaign.save
 
     user = User.find_by(id: session[:userid])
