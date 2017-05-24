efb10eda1a6a7ccc3b2c18fd20eefca5541f6c22
Joel Abrahams <abrahamsjo@gmail.com>
2017-05-24 15:49:23 +0200

    Fix conditional new image

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -4,34 +4,36 @@ class ImagesController < ApplicationController
     # depending on the campaign id, selects the next condition in the sequences, which links to a different table
     # which has a presentation time, and an image id, which links to an image path (unique for each image +
     # distortion level)
-    if !session[:training] && !session[:training].nil?
-      @img_num = session[:img_num].to_i
 
-      if @img_num < (session[:image_ids].length-1)
-        if @img_num == 5 || @img_num == 19
-          redirect_to content_path
-        else
-          session[:img_num] = @img_num + 1 #increment image id
-        end
-        #redirect_to show_path
-      else
-        redirect_to end_path
-      end
-
-      #if session[:img_num].to_i < session[:images].length
-      @score = Score.new
-      #set image to be shown
-      @img = Image.find(session[:image_ids][@img_num].to_i)
-      session[:view_time] = Viewtime.find(session[:view_time_ids][@img_num].to_i).viewtime
-
-      puts 'User user_id: [' + session[:userid].to_s + ']'
-      puts 'Viewing image_id: [' + session[:image_ids][@img_num].to_s+ '], image: [' +@img.filepath.to_s + ']'
-      puts 'With viewtime_id: [' + session[:view_time_ids][@img_num].to_s+ '], view time: [' +
-               session[:view_time].to_s + ']'
-    else
+    if session[:training] || session[:training].nil?
       @img = Image.find(-1)
       puts 'User shown training image: ' + @img.filepath.to_s
+      return
     end
+
+    @img_num = session[:img_num].to_i
+
+    if @img_num < (session[:image_ids].length-1)
+      if @img_num == 5 || @img_num == 19
+        redirect_to content_path
+      else
+        session[:img_num] = @img_num + 1 #increment image id
+      end
+      #redirect_to show_path
+    else
+      redirect_to end_path
+    end
+
+    #if session[:img_num].to_i < session[:images].length
+    @score = Score.new
+    #set image to be shown
+    @img = Image.find(session[:image_ids][@img_num].to_i)
+    session[:view_time] = Viewtime.find(session[:view_time_ids][@img_num].to_i).viewtime
+
+    puts 'User user_id: [' + session[:userid].to_s + ']'
+    puts 'Viewing image_id: [' + session[:image_ids][@img_num].to_s+ '], image: [' +@img.filepath.to_s + ']'
+    puts 'With viewtime_id: [' + session[:view_time_ids][@img_num].to_s+ '], view time: [' +
+             session[:view_time].to_s + ']'
   end
 
   private
