382b7407f28209aa1e2256a6171e467110e60bda
Joel Abrahams <abrahamsjo@gmail.com>
2017-07-20 10:19:31 +0200

    Show question after second validation question

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -5,6 +5,11 @@ class ImagesController < ApplicationController
     # which has a presentation time, and an image id, which links to an image path (unique for each image +
     # distortion level)
 
+    @img_num = session[:img_num].to_i
+    if @img_num >= (session[:image_ids].length - 1)
+      redirect_to end_path
+    end
+
     if session[:training] || session[:training].nil?
       @img = Image.find(-1)
       session[:view_time] = 107
@@ -12,24 +17,16 @@ class ImagesController < ApplicationController
       return
     end
 
-    @img_num = session[:img_num].to_i
-
-    if @img_num < (session[:image_ids].length-1)
-      if @img_num == 5 || @img_num == 12
-        redirect_to content_path
-      else
-        session[:img_num] = @img_num + 1 #increment image id
-      end
-      #redirect_to show_path
-    else
-      redirect_to end_path
-    end
-
-    #if session[:img_num].to_i < session[:images].length
     @score = Score.new
     #set image to be shown
     @img = Image.find(session[:image_ids][@img_num].to_i)
     session[:view_time] = Viewtime.find(session[:view_time_ids][@img_num].to_i).viewtime
+
+    if @img_num == 5 || @img_num == 12
+      redirect_to content_path
+    else
+      session[:img_num] = @img_num + 1 #increment image id
+    end
 
     puts 'User user_id: [' + session[:user_id].to_s + ']'
     puts 'Viewing image_id: [' + session[:image_ids][@img_num].to_s+ '], image: [' +@img.filepath.to_s + ']'
